98e0b3ec813806ea83eb30cccf71e88ebc623f98
hatsu38 <hajiwata0308@gmail.com>
2020-12-05 14:56:25 +0900

    Create LineBot Controller

---
+++ app/controllers/linebot/client_controller.rb
@@ -0,0 +1,32 @@
+class LineBot::ClientController < ApplicationController
+
+  def create
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    clinent = LineBot.client
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text'] + "新たに発生した患者数は2人です"
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # post '/callback' => 'linebot#callback'
+  namespace :linebot do
+    resources :client, only: :create
+  end
 end
