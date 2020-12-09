70be12d4619b1392cbc37ec0f89487bce8e94417
hatsu38 <hajiwata0308@gmail.com>
2020-12-09 20:39:26 +0900

    メッセージ変更と通知時間の更新を可能にした

--- app/models/remind_time.rb
+++ app/models/remind_time.rb
@@ -5,4 +5,9 @@ class RemindTime < ActiveYaml::Base
   set_filename "remind_time"
 
   enum_accessor :type
+
+  def self.find_time(time)
+    find_by(name_24: time) || find_by(name_12: time) ||
+    find_by(name_en_12: time) || find_by(time: time) || find_by(time_easy: time)
+  end
 end
